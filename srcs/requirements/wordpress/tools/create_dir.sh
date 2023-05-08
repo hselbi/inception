e4c0605a719b70e81b72bb4e3326f948059c771b
Hafid Selbi <hselbi@e1r12p7.1337.ma>
2023-05-08 20:17:06 +0100

    mandatory finished

--- srcs/requirements/wordpress/tools/create_dir.sh
+++ srcs/requirements/wordpress/tools/create_dir.sh
@@ -1,19 +1,16 @@
-#!/bin/bash
+cd /var/www/wordpress
 
-cd /var/www && curl https://wordpress.org/wordpress-5.7.2.tar.gz
+mv wp-config-sample.php wp-config.php
 
-tar -xzvf wordpress-5.7.2.tar.gz
+mv  /wp-config.php  ./wp-config.php
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+chmod -R 0777 wp-content/
+chmod -R 0777 wp-includes/
 
-mv /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+sed -i "s|'database_name_here'|${DB_NAME}|g" wp-config.php
+sed -i "s|'username_here'|${DB_USER}|g" wp-config.php
+sed -i "s|'password_here'|${DB_PASSWORD}|g" wp-config.php
+sed -i "s|'localhost'|${DB_HOST}|g" wp-config.php
+sed -i "s|'password_here'|${DB_PASSWORD}|g" wp-config.php
 
-mv /wp-config.php /var/www/wordpress/wp-config.php
-
-sed -i "s|'database_name_here'|${DB_NAME}|g" /var/www/wordpress/wp-config.php
-sed -i "s|'username_here'|${DB_USER}|g" /var/www/wordpress/wp-config.php
-sed -i "s|'password_here'|${DB_PASSWORD}|g" /var/www/wordpress/wp-config.php
-sed -i "s|'localhost'|${DB_HOST}|g" /var/www/wordpress/wp-config.php
 
